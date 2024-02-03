9bedf8e75df26665f8a47a5f5109321dd125f5ca
h.s.gujarathi750 <h.s.gujarathi750@gmail.com>
2024-02-03 23:12:59 +0530

    first commit

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-undef */
 import './Intro'
+
 const Navigation = () => {
 
     return (<nav>
         <div>
-            <img src="./Harshal.gif" />
+            <img src="//public/logo.gif" />
             <ul>
                 <li href="#">Menu</li>
                 <li href="#">Location</li>
