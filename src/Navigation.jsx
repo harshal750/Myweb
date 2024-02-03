feb9b2fe333cdd6b3d5f1c80cc812fa424c9e057
h.s.gujarathi750 <h.s.gujarathi750@gmail.com>
2024-02-03 20:01:19 +0530

    first commit

---
+++ src/Navigation.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-undef */
+import './Intro'
+const Navigation = () => {
+
+    return (<nav>
+        <div>
+            <img src="./Harshal.gif" />
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+
+            </ul>
+        </div>
+    </nav>);
+};
+
+export default Navigation;
